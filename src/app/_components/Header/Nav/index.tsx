8509565c2192f36d2584dbed0838ffcb22eaa389
dbahiense <douglas@rohling.com.br>
2024-08-24 16:24:58 -0300

    fix: delete unnecessary cart links from nav

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link'
 import { Header as HeaderType } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
 import { Button } from '../../Button'
-import { CartLink } from '../../CartLink'
 import { CMSLink } from '../../Link'
 
 import classes from './index.module.scss'
@@ -20,8 +19,11 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="none" />
       })}
-      <CartLink />
-      {user && <Link href="/account">Account</Link>}
+      {user && (
+        <Link href="/account" className={classes.link}>
+          Minha Conta
+        </Link>
+      )}
       {!user && (
         <Button
           el="link"
@@ -31,7 +33,6 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
           onClick={() => (window.location.href = '/login')}
         />
       )}
-      {user && <CartLink />}
     </nav>
   )
 }
