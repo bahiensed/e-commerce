ae9646e3de4ce3b28e379fe90c855938609a68c6
dbahiense <douglas@rohling.com.br>
2024-09-15 12:46:14 -0300

    fix: add `targetData` to the dependency array

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -36,7 +36,7 @@ const Promotion = () => {
     return () => {
       clearInterval(timerInterval) // Cleanup the interval when the component unmounts.
     }
-  }, [])
+  }, [targetDate])
 
   return (
     <section className={classes.promotion}>
